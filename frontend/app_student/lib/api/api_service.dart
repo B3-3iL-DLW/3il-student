241f4039ee8950f1ea778106dfa29bad15ca8f5f
Jules <jules.artaud@outlook.com>
2024-03-12 17:16:59 +0100

    ♻️: remove print linter

--- frontend/app_student/lib/api/api_service.dart
+++ frontend/app_student/lib/api/api_service.dart
@@ -11,25 +11,16 @@ class ApiService {
       String endpoint, T Function(Map<String, dynamic>) fromJson) async {
     String fullUrl = '$apiUrl$endpoint';
     fullUrl = Uri.encodeFull(fullUrl);
-    print(fullUrl);
     try {
       final response = await http.get(Uri.parse(fullUrl));
 
       if (response.statusCode == 200) {
         List jsonResponse = json.decode(response.body);
-        print('JSON Response: $jsonResponse');
-
         return jsonResponse.map((item) {
-          print('Item: $item'); // Print the item before conversion
           try {
             return fromJson(item);
           } catch (e) {
-            print(
-                'Failed to convert item: $e'); // Print the error if conversion fails
-            print(
-                'Failed item: $item'); // Print the item that failed to convert
-
-            throw e;
+            rethrow;
           }
         }).toList();
       } else {
