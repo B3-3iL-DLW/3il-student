4bf85eaf69f9e77ee941d6e8536c6647b39b552a
Jules Artaud <jules.artaud@outlook.com>
2024-03-12 11:26:57 +0100

    🐛: fix week schedule vie

--- frontend/app_student/lib/week_schedule/views/week_schedule.dart
+++ frontend/app_student/lib/week_schedule/views/week_schedule.dart
@@ -1,10 +1,50 @@
+import 'package:app_student/api/day_schedule/entities/day_schedule_entity.dart';
+import 'package:app_student/api/week_schedule/models/week_schedule_model.dart';
 import 'package:app_student/api/week_schedule/repositories/week_schedule_repositories.dart';
 import 'package:app_student/week_schedule/cubit/week_schedule_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+class DayScheduleWidget extends StatelessWidget {
+  final DayScheduleEntity daySchedule;
+
+  const DayScheduleWidget({Key? key, required this.daySchedule})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text('Date: ${daySchedule.date}'),
+      children: daySchedule.events.map((event) {
+        return ListTile(
+          title: Text('Event: ${event.activite}'),
+          subtitle: Text(
+              'Start at: ${event.horaires.startAt}, End Time: ${event.horaires.endAt}'),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class WeekScheduleWidget extends StatelessWidget {
+  final WeekScheduleModel weekSchedule;
+
+  const WeekScheduleWidget({Key? key, required this.weekSchedule})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text('Code: ${weekSchedule.code}'),
+      children: weekSchedule.daySchedules.map((daySchedule) {
+        return DayScheduleWidget(daySchedule: daySchedule);
+      }).toList(),
+    );
+  }
+}
+
 class WeekSchedulePage extends StatelessWidget {
-  const WeekSchedulePage({super.key});
+  const WeekSchedulePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +68,8 @@ class WeekSchedulePage extends StatelessWidget {
               return ListView.builder(
                 itemCount: state.weekSchedule.length,
                 itemBuilder: (context, index) {
-                  final WeekSchedule = state.weekSchedule[index];
-                  return ListTile(
-                    title: Text('Code: ${WeekSchedule.code}'),
-                    // subtitle: Column(
-                    //   crossAxisAlignment: CrossAxisAlignment.start,
-                    //   children: WeekSchedule.events.map((event) {
-                    //     return Text(
-                    //         'Event: ${event.activite}, Start at: ${event.horaires.startAt}, End Time: ${event.horaires.endAt}');
-                    //   }).toList(),
-                    // ),
-                  );
+                  final weekSchedule = state.weekSchedule[index];
+                  return WeekScheduleWidget(weekSchedule: weekSchedule);
                 },
               );
             } else if (state is WeekScheduleError) {
