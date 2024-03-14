cb6937714c788e33994b9a99666b89ff425be6e8
Lukas <35173609+PHPLukaas@users.noreply.github.com>
2024-03-14 11:19:30 +0100

    🚨: ok yan merge fixed lol

--- frontend/app_student/lib/main_dev.dart
+++ frontend/app_student/lib/main_dev.dart
@@ -1,7 +1,6 @@
 import 'package:app_student/config/dev_config.dart';
 import 'package:app_student/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
@@ -9,8 +8,7 @@ import 'package:provider/provider.dart';
 
 import 'config/config.dart';
 
-void main() async {
-  await dotenv.load();
+void main() {
   runApp(
     Provider<Config>(
       create: (_) => DevConfig(),
