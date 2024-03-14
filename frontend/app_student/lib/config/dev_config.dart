afc64fc9fe538412039eb1796e452b096992cdb5
Lukas <35173609+PHPLukaas@users.noreply.github.com>
2024-03-14 11:19:30 +0100

    🚨: ok yan merge fixed lol

--- frontend/app_student/lib/config/dev_config.dart
+++ frontend/app_student/lib/config/dev_config.dart
@@ -1,8 +1,6 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'config.dart';
 
 class DevConfig extends Config {
   @override
-  String get apiUrl => dotenv.env['DEV_API_URL'] ?? 'http://localhost:8000';
+  String get apiUrl => 'https://api-dev.lukasvalois.com';
 }
--- frontend/app_student/lib/config/prod_config.dart
+++ frontend/app_student/lib/config/prod_config.dart
@@ -1,8 +1,6 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'config.dart';
 
 class ProdConfig extends Config {
   @override
-  String get apiUrl => dotenv.env['PROD_API_URL'] ?? 'http://localhost:8000';
+  String get apiUrl => 'https://api.lukasvalois.com';
 }
