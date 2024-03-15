e5226216ffda9d0fa90a3ce896f7f9558fa843ff
Lukas <lukasvalois@gmail.com>
2024-03-15 19:39:07 +0100

    🚑️: Fix empty rooms

--- backend/src/Service/TimetableService.php
+++ backend/src/Service/TimetableService.php
@@ -85,11 +85,16 @@ class TimetableService
 
                 foreach ($day['CRENEAU'] as $creneau) {
                     if (isset($creneaux[$creneau['Creneau']])) {
+
+                        // Verification si la salle est un array vide car le XML de 3il est nul
+                        if (is_array($creneau['Salles'] ?? '')) {
+                            $creneau['Salles'] = '';
+                        }
+
                         $eventHours = new EventHours();
                         $eventHours->setStartAt($creneaux[$creneau['Creneau']]['start']);
                         $eventHours->setEndAt($creneaux[$creneau['Creneau']]['end']);
                         $eventHours->setId(random_int(0, 1000000));
-
                         $event = new Event();
                         $event->setCreneau($creneau['Creneau']);
                         $event->setActivite($creneau['Activite'] ?? 'Pas cours');
