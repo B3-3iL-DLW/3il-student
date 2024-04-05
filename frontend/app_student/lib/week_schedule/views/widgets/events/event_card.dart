b4a0112ee7e757b4dffbad8fe8d3ac51a4f261cb
Jules Artaud <jules.artaud@outlook.com>
2024-04-05 18:48:52 +0200

    🐛: fix auto size card event

--- frontend/app_student/lib/week_schedule/views/widgets/events/event_card.dart
+++ frontend/app_student/lib/week_schedule/views/widgets/events/event_card.dart
@@ -15,59 +15,60 @@ class EventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Color(0xFF007A8D),
-          border: Border(
-            left: BorderSide(
-              color: CustomTheme.primaryColor,
-              width: 10.0,
+      child: IntrinsicHeight(
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF007A8D),
+            border: Border(
+              left: BorderSide(
+                color: CustomTheme.primaryColor,
+                width: 10.0,
+              ),
             ),
           ),
-        ),
-        height: 110,
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 15.0, right: 20, bottom: 3, top: 5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: event.creneau == 3
-                ? MainAxisAlignment.center
-                : MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Text(AppLocalizations.of(context)!.eventDuration,
-                    style: CustomTheme.textSmall.toColorWhite),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                      child: Text(event.activite,
-                          style: CustomTheme.textXl.toBold.toColorWhite)),
-                  ColorFiltered(
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                    child: SvgPicture.asset(
-                      event.visio
-                          ? 'assets/images/teams.svg'
-                          : 'assets/images/school.svg',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                ],
-              ),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                      '${AppLocalizations.of(context)!.roomLabel} ${event.salle}',
-                      style: CustomTheme.text.toColorWhite),
+          child: Padding(
+            padding:
+            const EdgeInsets.only(left: 15.0, right: 20, bottom: 3, top: 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: event.creneau == 3
+                  ? MainAxisAlignment.center
+                  : MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(AppLocalizations.of(context)!.eventDuration,
+                      style: CustomTheme.textSmall.toColorWhite),
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                        child: Text(event.activite,
+                            style: CustomTheme.textXl.toBold.toColorWhite)),
+                    ColorFiltered(
+                      colorFilter:
+                      const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                      child: SvgPicture.asset(
+                        event.visio
+                            ? 'assets/images/teams.svg'
+                            : 'assets/images/school.svg',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                  ],
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                        '${AppLocalizations.of(context)!.roomLabel} ${event.salle}',
+                        style: CustomTheme.text.toColorWhite),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
